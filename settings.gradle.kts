8b3cf993a873e8047d7e6e199aa559e44d356760
aprilia wulandari <aprlawlndr97@gmail.com>
2023-11-28 10:10:12 +0700

    Initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "BR_Notification_14"
+include(":app")
+ 
